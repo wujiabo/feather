c0b15042abc2bfa80c467c347efa964cf5a19f67
Jiabo.Wu <wujiabo@126.com>
2016-02-19 17:30:07 +0800

    2016-02-19

--- src/main/java/com/wujiabo/opensource/feather/web/controller/IndexController.java
+++ src/main/java/com/wujiabo/opensource/feather/web/controller/IndexController.java
@@ -1,16 +1,13 @@
 package com.wujiabo.opensource.feather.web.controller;
 
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
-import org.apache.shiro.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.wujiabo.opensource.feather.constants.Constants;
 import com.wujiabo.opensource.feather.model.TUser;
 import com.wujiabo.opensource.feather.service.RbacService;
 import com.wujiabo.opensource.feather.web.bind.annotation.CurrentUser;
@@ -28,13 +25,6 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index(@CurrentUser TUser loginUser, Model model) {
-    //    model.addAttribute("menus", menus);
-    	Session shiroSession = SecurityUtils.getSubject().getSession();
-    	
-    	if(shiroSession.getAttribute(Constants.CURRENT_MENU) == null){
-    		String currentMenuJson = rbacService.getCurrentMenuJson(loginUser.getUserId());
-        	shiroSession.setAttribute(Constants.CURRENT_MENU, currentMenuJson);
-    	}
         return "index";
     }
 
@@ -45,7 +35,7 @@ public class IndexController {
     @ResponseBody
     @RequiresRoles(value = "ADMIN")
     public String admin() {
-        return "拥有角色,能访问";
+        return "Has Role";
     }
 
     /**
@@ -55,7 +45,7 @@ public class IndexController {
     @ResponseBody
     @RequiresPermissions(value = "USER_CREATE")
     public String create() {
-        return "拥有权限,能访问";
+        return "Has Permission";
     }
 
 
--- src/main/java/com/wujiabo/opensource/feather/web/shiro/filter/SysUserFilter.java
+++ src/main/java/com/wujiabo/opensource/feather/web/shiro/filter/SysUserFilter.java
@@ -4,10 +4,12 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.web.filter.PathMatchingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.wujiabo.opensource.feather.constants.Constants;
+import com.wujiabo.opensource.feather.model.TUser;
 import com.wujiabo.opensource.feather.service.RbacService;
 
 /**
@@ -24,7 +26,17 @@ public class SysUserFilter extends PathMatchingFilter {
     protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
 
         String username = (String)SecurityUtils.getSubject().getPrincipal();
-        request.setAttribute(Constants.CURRENT_USER, rbacService.findByUsername(username));
+        TUser user = rbacService.findByUsername(username);
+        request.setAttribute(Constants.CURRENT_USER, user);
+        
+
+    	Session shiroSession = SecurityUtils.getSubject().getSession();
+    	
+    	if(shiroSession.getAttribute(Constants.CURRENT_MENU) == null){
+    		String currentMenuJson = rbacService.getCurrentMenuJson(user.getUserId());
+        	shiroSession.setAttribute(Constants.CURRENT_MENU, currentMenuJson);
+    	}
+        
         return true;
     }
 }
